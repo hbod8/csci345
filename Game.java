875c380ea0d4da51c2d00c3527b09798f79c75e4
Thomas Bidinger <bidingt@wwu.edu>
2020-05-08 23:09:20 -0700

    partially implemented program

--- Game.java
+++ Game.java
@@ -1,23 +1,27 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.lang.Exception;
+
 public class Game {
-  private LinkedList<Player> players;
+  private static LinkedList<Player> players;
   private int days;
   private int maxDays;
   private LinkedList<Room> rooms;
-  private Scanner s;
+  private static Scanner s;
   public Game(int playerCount) throws Exception {
     if (playerCount < 4) {
       this.maxDays = 3;
-      for
+      //this was causing an error
     }
 
   }
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     s = new Scanner(System.in);
     System.out.println("Welcome to Deadwood!");
-    System.out.print("How many players? >");
+    System.out.print("How many players? > ");
     int playerCount = s.nextInt();
     System.out.print("\n");
-    if (players > 8 || players < 2) {
+    if (players.size() > 8 || players.size() < 2) {
       throw new Exception("Error: you can only have 2-8 players.");
     }
     Game activeGame = new Game(playerCount);
